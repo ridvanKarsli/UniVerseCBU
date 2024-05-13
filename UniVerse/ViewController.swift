8a22f6d5872cb7077e88105d34a45cf7d5ba7972
Rıdvan Şevki Karslı <rdvn.35050@gmail.com>
2024-05-13 08:21:45 +0300

    Initial Commit

---
+++ UniVerse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniVerse
+//
+//  Created by Rıdvan KARSLI on 13.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
